538815c60be02f66c3897b41f88fc23965640714
Tayfun Bocek <tayfunbocek@live.ca>
2025-10-23 00:12:12 +0300

    remove redundant comment

--- queries/locals.scm
+++ queries/locals.scm
@@ -4,7 +4,6 @@
 (array_declarator declarator: (identifier) @local.definition)
 (parameter_declaration declarator: (identifier) @local.definition)
 (expression [left: (identifier) @local.reference 
-                                           ; use expression/identifier as it means any node in the right field is a valid construct on its own
                                              argument: (identifier) @local.reference])
 (expression/identifier) @local.reference
 
