dfc875e55ebb9143d00515912b70fcd7428577db
Tayfun Bocek <tayfunbocek@live.ca>
2025-10-22 10:35:32 +0300

    add locals

---
+++ queries/locals.scm
@@ -0,0 +1,9 @@
+(declaration) @local.definition
+(function_definition) @local.scope
+(parameter_declaration) @local.definition
+(identifier) @local.reference
+
+(while_statement) @local.scope
+(do_statement) @local.scope
+(if_statement) @local.scope
+(else_clause) @local.scope
