3d88386ad1fb2a0c6bdc9a803231fbade91902af
Tayfun Bocek <tayfunbocek@live.ca>
2025-10-23 00:11:04 +0300

    fmt

--- queries/tags.scm
+++ queries/tags.scm
@@ -1,4 +1,4 @@
 (function_definition declarator: (identifier) @name) @definition.function
 (call_expression function: (identifier) @name ) @reference.call
 (struct_definition name: (identifier) @name) @definition.type
-((type_identifier) @name)@reference.type
+((type_identifier) @name) @reference.type
