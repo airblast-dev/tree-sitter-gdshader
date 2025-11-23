68268631c8b6dc093985f1246b099f81b30ea7d1
Tayfun Bocek <tayfunbocek@live.ca>
2025-11-24 00:49:16 +0300

    bump version

--- src/parser.c
+++ src/parser.c
@@ -24335,7 +24335,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_gdshader(void) {
     .max_reserved_word_set_size = 27,
     .metadata = {
       .major_version = 0,
-      .minor_version = 3,
+      .minor_version = 4,
       .patch_version = 0,
     },
   };
